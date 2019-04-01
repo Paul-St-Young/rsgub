b1d8b4022afa280a1ac0d0c4d1d2cc9084634412
Paul Young <yyang173@illinois.edu>
2019-04-01 10:10:14 -0500

    add write_cube and finish RegularGrid3D

--- rsgub/grids/grid3d.py
+++ rsgub/grids/grid3d.py
@@ -1,20 +1,27 @@
+import os
 import numpy as np
 
 class RegularGrid3D:
-  def __init__(self, gmin=None, dg=None, ng=None, cols=None):
+  def __init__(self, ng, ncol=1, dtype=float, gmin=None, dg=None):
     """Create a regular grid of scalar data.
     The grid is defined by its (origin, spacing, # of points).
     The scalar data are stored as columns.
 
     Args:
+      ng (np.array): number of grid points (3,) vector of ints
+      ncol (int, optional): number of data columns, default is 1
+      dtype (type): type of data columns, default is float
       gmin (np.array, optional): origin (3,) vector of floats
       dg (np.array, optional): spacing (3,) vector of floats
-      ng (np.array, optional): number of grid points (3,) vector of ints
     """
+    # define grid
     self._gmin = gmin
     self._dg = dg
     self._ng = ng
-    self._cols = cols
+    # add empty columns
+    ntot = np.prod(ng)  # total number of grid points
+    self._cols = np.zeros([ntot, ncol], dtype=dtype)
+    self._cols[:] = np.nan
   def _initialized(self):
     """Check if the grid structure is defined
 
@@ -23,8 +30,7 @@ class RegularGrid3D:
     """
     initialized = np.all([
       self._gmin is not None,
-      self._dg is not None,
-      self._ng is not None
+      self._dg is not None
     ])
     return initialized
   def get_grid(self):
@@ -41,12 +47,19 @@ class RegularGrid3D:
     ).reshape(-1, 3)
     rgvecs = self._gmin + self._dg*upos
     return rgvecs
+  def get_col(self, icol=0):
+    """Get a column of data from the regular grid
+
+    Return:
+      np.array: vals, array of scalars
+    """
+    return self._cols[:, icol]
   def find(self, qvec, tol=1e-2):
-    """Find the index for a grid point
+    """Find the index for a grid point.
 
     Args:
       qvec (np.array): grid point to be found
-      tol (float, optional): tolerance on integer match, default 1e-2
+      tol (float, optional): tolerance for integer match, default 1e-2
     Return:
       int: index
     """
@@ -67,10 +80,6 @@ class RegularGrid3D:
     """
     if not self._initialized():
       raise RuntimeError('must initialize before adding data')
-    if self._cols is None:  # add first column
-      ntot = np.prod(self._ng)
-      self._cols = np.empty([ntot, icol+1])
-      self._cols[:] = np.nan
     for qvec, val in zip(qvecs, vals):
       # find closest grid point
       idx = self.find(qvec, tol=tol)
@@ -87,13 +96,13 @@ class RegularGrid3D:
     volumetric.color_scatter(ax, rgvecs[sel], vals[sel])
     plt.show()
   def init_from_cube(self, fcube, force=False):
-    from qharv.inspect import volumetric
+    from qharv.inspect.volumetric import read_gaussian_cube
     if self._initialized and (not force):
       msg = 'refusing to initialize twice;'
       msg += ' use force if you have to.'
       raise RuntimeError(msg)
     # read cube file
-    entry = volumetric.read_gaussian_cube(fcube)
+    entry = read_gaussian_cube(fcube)
     # initialize internal variables
     rgrid = entry['data']
     raxes = entry['axes']
@@ -102,3 +111,15 @@ class RegularGrid3D:
     self._dg = np.diag(raxes)
     self._gmin = origin
     self._cols = rgrid.ravel()[:, np.newaxis]
+  def write_cube(self, fcube, icol=0, force=False):
+    if os.path.isfile(fcube):
+      msg = 'refusing to overwrite %s;' % fcube
+      msg += ' use force if you have to.'
+      raise RuntimeError(msg)
+    from qharv.inspect.volumetric import write_gaussian_cube
+    vals = self._cols[:, icol]
+    vol = vals.reshape(self._ng)
+    axes = np.diag(self._dg)
+    text = write_gaussian_cube(vol, axes, origin=self._gmin)
+    with open(fcube, 'w') as f:
+      f.write(text)
