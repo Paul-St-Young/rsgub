2950318fe188829c010dc189bd0351f3debaa765
Paul Young <yyang173@illinois.edu>
2019-05-08 17:13:42 -0500

    simplify logic of pbc_unit_box

--- rsgub/unfold/pbc.py
+++ rsgub/unfold/pbc.py
@@ -1,4 +1,8 @@
 import numpy as np
+
+def pos_in_box(pos, lbox):
+  return (pos+lbox/2.) % lbox - lbox/2.
+
 def pbc_unit_box(rsg, icol=0, atol=1e-3):
   """Apply periodic boundary condition (PBC) to regular grid.
 
@@ -22,36 +26,22 @@ def pbc_unit_box(rsg, icol=0, atol=1e-3):
   ng1 = np.array(rsg.get_ng())+2
   gmin1 = gmin-dg
   rsg1 = RegularGrid3D(ng1, gmin=gmin1, dg=dg)
-  # add PBC data
-  for gside, gshift in zip([gmin, gmax], [1, -1]):
-    #  1. mirror planes
-    for idim in range(ndim):
-      xmin = gside[idim]
-      zsel = abs(kvecs[:, idim]-xmin) < atol
-      shift = np.zeros([ndim])
-      shift[idim] = gshift
-      rsg1.add(kvecs[zsel]+shift, vals[zsel], icol=icol)
-    #  2. mirror edges
-    for idim in range(ndim):
-      xmin = gside[idim]
-      zsel = abs(kvecs[:, idim]-xmin) < atol
-      zsel = zsel & (abs(kvecs[:, (idim+1) % ndim]-xmin) < 1e-3)
-      shift = np.zeros([ndim])
-      shift[idim] = gshift
-      shift[(idim+1) % ndim] = gshift
-      rsg1.add(kvecs[zsel]+shift, vals[zsel], icol=icol)
-    #  3. mirror cornor
-    zsel = np.all(abs(kvecs-gside) < atol, axis=1)
-    shift = gshift*np.ones([ndim])
-    rsg1.add(kvecs[zsel]+shift, vals[zsel], icol=icol)
-  # end for gside, gshift
-
   # add original data
   rsg1.add(kvecs, vals, icol=icol)
-  ## check success
-  #vals1 = rsg1.get_col()
-  #success = ~np.isnan(vals1).any()
-  #assert success
+  # add PBC data
+  vals1 = rsg1.get_col()
+  nsel = np.isnan(vals1)
+  myidx = np.arange(len(vals1))[nsel]
+  kvecs2fill = rsg1.get_grid()[nsel]
+  for i, kvec in enumerate(kvecs2fill):
+    kvec1 = pos_in_box(kvec, 1.)
+    idx = rsg.find(kvec1)
+    vals1[myidx[i]] = vals[idx]
+  rsg1.set_col(vals1, 0)
+  # check success
+  vals1 = rsg1.get_col()
+  success = ~np.isnan(vals1).any()
+  assert success
   return rsg1
 
 def pbc_unit_box_one_side(rsg, atol=1e-3):
